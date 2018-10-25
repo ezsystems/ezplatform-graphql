92af1935e937201d80037a1a0dc390ec35348c56
Bertrand Dunogier <bertrand.dunogier@ez.no>
2018-10-25 18:21:07 +0200

    Changed basic fields types to be mapped to scalar types
    Implements #29
    
    Applies to ezstring, eztext, ezboolean, ezfloat and ezinteger.
    Also removes the custom FieldValue types added for those types.

--- DomainContent/FieldValueBuilder/BaseFieldValueBuilder.php
+++ DomainContent/FieldValueBuilder/BaseFieldValueBuilder.php
@@ -5,24 +5,36 @@ use eZ\Publish\API\Repository\Values\ContentType\FieldDefinition;
 
 class BaseFieldValueBuilder implements FieldValueBuilder
 {
+    const DEFAULT_RESOLVER = '@=resolver("DomainFieldValue", [value, "%s"])';
+
     private $typesMap = [
         'ezauthor' => 'AuthorFieldValue',
         'ezgmaplocation' => 'MapLocationFieldValue',
         'ezimage' => 'ImageFieldValue',
         'ezrichtext' => 'RichTextFieldValue',
-        'ezstring' => 'TextLineFieldValue',
+        'ezfloat' => ['Float', '@=resolver("DomainFieldValue", [value, "%s"]).value'],
+        'ezinteger' => ['Int', '@=resolver("DomainFieldValue", [value, "%s"]).value'],
+        'ezboolean' => ['Boolean', '@=resolver("DomainFieldValue", [value, "%s"]).bool'],
+        'ezstring' => ['String', '@=resolver("DomainFieldValue", [value, "%s"]).text'],
+        'eztext' => ['String', '@=resolver("DomainFieldValue", [value, "%s"]).text'],
         'ezobjectrelation' => 'RelationFieldValue',
         'ezobjectrelationlist' => 'RelationListFieldValue',
     ];
 
     public function buildDefinition(FieldDefinition $fieldDefinition)
     {
+        $mapping = $this->mapFieldTypeIdentifierToGraphQLType($fieldDefinition->fieldTypeIdentifier);
+
+        if (is_array($mapping)) {
+            list($type, $resolver) = $mapping;
+        } else {
+            $type = $mapping;
+            $resolver = self::DEFAULT_RESOLVER;
+        }
+
         return [
-            'type' => $this->mapFieldTypeIdentifierToGraphQLType($fieldDefinition->fieldTypeIdentifier),
-            'resolve' => sprintf(
-                '@=resolver("DomainFieldValue", [value, "%s"])',
-                $fieldDefinition->identifier
-            ),
+            'type' => $type,
+            'resolve' => sprintf($resolver, $fieldDefinition->identifier),
         ];
     }
 
