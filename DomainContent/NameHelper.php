0972b083225b59e2d59f1b32229d7889b7d0b64a
Bertrand Dunogier <bertrand.dunogier@ez.no>
2018-10-26 16:05:11 +0200

    Improved pluralization rules (#31)

--- DomainContent/NameHelper.php
+++ DomainContent/NameHelper.php
@@ -28,7 +28,7 @@ class NameHelper
 
     public function domainContentCollectionField(ContentType $contentType)
     {
-        return lcfirst($this->toCamelCase($contentType->identifier)) . 's';
+        return $this->pluralize(lcfirst($this->toCamelCase($contentType->identifier)));
     }
     public function domainContentName(ContentType $contentType)
     {
@@ -58,5 +58,50 @@ class NameHelper
     private function toCamelCase($string)
     {
         return $this->caseConverter->denormalize($string);
+    }
+
+    private function pluralize($name)
+    {
+        if (substr($name, -1) === 'f') {
+            return substr($name, 0, -1) . "ves";
+        }
+
+        if (substr($name, -1) === 'fe') {
+            return substr($name, 0, -2) . "ves";
+        }
+
+        if (substr($name, -1) === 'y') {
+
+            if (in_array(substr($name, -2, 1), ['a', 'e', 'i', 'o', 'u'])) {
+                return $name . 's';
+            } else {
+                return substr($name, 0, -1) . "ies";
+            }
+        }
+
+        if (substr($name, -2) === 'is') {
+            return substr($name, 0, -2) . "es";
+        }
+
+        if (substr($name, -2) === 'us') {
+            return substr($name, 0, -2) . "i";
+        }
+
+        if (in_array(substr($name, -2), ['on', 'um'])) {
+            return substr($name, 0, -2) . "a";
+        }
+
+        if (substr($name, -2) === 'is') {
+            return substr($name, 0, -2) . "es";
+        }
+
+        if (
+            preg_match('/(s|sh|ch|x|z)$/', $name) ||
+            substr($name, -1) ===  'o'
+        ) {
+            return $name . 'es';
+        }
+
+        return $name . 's';
     }
 }
