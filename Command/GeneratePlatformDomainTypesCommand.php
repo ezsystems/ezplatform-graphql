9c23a23396f180a53e4e6b317b914c9ce836aa25
Bertrand Dunogier <bertrand.dunogier@ez.no>
2018-10-28 16:09:54 +0100

    Added debug features to generate command
    Use --dry-run to echo the yaml definition without writing.
    Use --include=<type> to limit output to a type. Can be repeated.

--- Command/GeneratePlatformDomainTypesCommand.php
+++ Command/GeneratePlatformDomainTypesCommand.php
@@ -9,6 +9,7 @@ use BD\EzPlatformGraphQLBundle\Schema\SchemaGenerator;
 use eZ\Publish\API\Repository\Repository;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Yaml\Yaml;
@@ -36,21 +37,33 @@ class GeneratePlatformDomainTypesCommand extends Command
 
     protected function configure()
     {
-        $this->setName('bd:platform-graphql:generate-domain-schema');
+        $this
+            ->setName('bd:platform-graphql:generate-domain-schema')
+            ->addOption('dry-run', null, InputOption::VALUE_OPTIONAL, '', false)
+            ->addOption('include', null, InputOption::VALUE_REQUIRED|InputOption::VALUE_IS_ARRAY, '', []);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $schema = $this->generator->generate();
 
+        $include = $input->getOption('include');
+        $doWrite = $input->getOption('dry-run') === false;
+
         $fs = new Filesystem();
         foreach ($schema as $type => $definition) {
+            if (count($include) && !in_array($type, $include)) {
+                continue;
+            }
             $typeFilePath = self::TYPES_DIRECTORY . "/$type.types.yml";
-            $fs->dumpFile(
-                $typeFilePath,
-                Yaml::dump([$type => $definition], 6)
-            );
-            $output->writeln("Written $typeFilePath");
+
+            $yaml = Yaml::dump([$type => $definition], 6);
+            if ($doWrite) {
+                $fs->dumpFile($typeFilePath, $yaml);
+                $output->writeln("Written $typeFilePath");
+            } else {
+                $output->writeln("\n# $type\n$yaml\n");
+            }
         }
     }
 }
