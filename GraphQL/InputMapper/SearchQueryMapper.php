3bda7e04279f84369ebb21d6ab6c996934bd3959
Bertrand Dunogier <bertrand.dunogier@ez.no>
2019-01-16 21:05:07 +0100

    Made the Field query filter a list:
    `products(query: {Field: [{target: "foo", eq: "bar"}, {target: "morefoo", ne: "morebar"}]}) { ... }`

--- GraphQL/InputMapper/SearchQueryMapper.php
+++ GraphQL/InputMapper/SearchQueryMapper.php
@@ -26,19 +26,19 @@ class SearchQueryMapper
 
         if (isset($inputArray['Field']))
         {
-            $args = $inputArray['Field'];
-            foreach (['in', 'eq', 'like', 'contains', 'between', 'lt', 'lte', 'gt', 'gte'] as $opString) {
-                if (isset($args[$opString])) {
-                    $value = $args[$opString];
-                    $operator = constant('eZ\Publish\API\Repository\Values\Content\Query\Criterion\Operator::' . strtoupper($opString));
-                }
+            if (isset($inputArray['Field']['target'])) {
+                $criteria[] = $this->mapInputToFieldCriterion($inputArray['Field']);
+            } else {
+                $criteria = array_merge(
+                    $criteria,
+                    array_map(
+                        function($input) {
+                            return $this->mapInputToFieldCriterion($input);
+                        },
+                        $inputArray['Field']
+                    )
+                );
             }
-
-            if (!isset($operator)) {
-                throw new InvalidArgumentException("Unspecified operator");
-            }
-
-            $criteria[] = new Query\Criterion\Field($args['target'], $operator, $value);
         }
 
         $criteria = array_merge($criteria, $this->mapDateMetadata($inputArray, 'Modified'));
@@ -129,4 +129,20 @@ class SearchQueryMapper
 
         return $criteria;
     }
+
+    private function mapInputToFieldCriterion($input)
+    {
+        $operators = ['in', 'eq', 'like', 'contains', 'between', 'lt', 'lte', 'gt', 'gte'];
+        foreach ($operators as $opString) {
+            if (isset($input[$opString])) {
+                $value = $input[$opString];
+                $operator = constant('eZ\Publish\API\Repository\Values\Content\Query\Criterion\Operator::' . strtoupper($opString));
+            }
+        }
+
+        if (!isset($operator)) {
+            throw new InvalidArgumentException("Unspecified operator");
+        }
+
+        return new Query\Criterion\Field($input['target'], $operator, $value);    }
 }
