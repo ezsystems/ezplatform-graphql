986b9c9996033e4a63eb7345756f038a17826b47
Maciej Kobus <webhdx@users.noreply.github.com>
2019-03-19 17:51:25 +0100

    EZP-30283: Implement fetching Object States in GraphQL (#11)

---
+++ src/GraphQL/Resolver/ObjectStateGroupResolver.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace EzSystems\EzPlatformGraphQL\GraphQL\Resolver;
+
+use eZ\Publish\API\Repository\Exceptions\NotFoundException;
+use eZ\Publish\API\Repository\ObjectStateService;
+use eZ\Publish\API\Repository\Values\ObjectState\ObjectStateGroup;
+use GraphQL\Error\UserError;
+use Overblog\GraphQLBundle\Definition\Argument;
+
+class ObjectStateGroupResolver
+{
+    /** @var \eZ\Publish\API\Repository\ObjectStateService */
+    private $objectStateService;
+
+    /**
+     * @param \eZ\Publish\API\Repository\ObjectStateService $objectStateService
+     */
+    public function __construct(ObjectStateService $objectStateService)
+    {
+        $this->objectStateService = $objectStateService;
+    }
+
+    /**
+     * @param \Overblog\GraphQLBundle\Definition\Argument $args
+     *
+     * @return \eZ\Publish\API\Repository\Values\ObjectState\ObjectStateGroup
+     */
+    public function resolveObjectStateGroupById(Argument $args): ObjectStateGroup
+    {
+        try {
+            return $this->objectStateService->loadObjectStateGroup($args['id']);
+        } catch (NotFoundException $e) {
+            throw new UserError("Object State Group with ID: {$args['id']} was not found.");
+        }
+    }
+
+    /**
+     * @return \eZ\Publish\API\Repository\Values\ObjectState\ObjectStateGroup[]
+     */
+    public function resolveObjectStateGroups(): array
+    {
+        return $this->objectStateService->loadObjectStateGroups();
+    }
+}
---
+++ src/GraphQL/Resolver/ObjectStateResolver.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace EzSystems\EzPlatformGraphQL\GraphQL\Resolver;
+
+use eZ\Publish\API\Repository\Exceptions\NotFoundException;
+use eZ\Publish\API\Repository\ObjectStateService;
+use eZ\Publish\API\Repository\Values\Content\ContentInfo;
+use eZ\Publish\API\Repository\Values\ObjectState\ObjectState;
+use eZ\Publish\API\Repository\Values\ObjectState\ObjectStateGroup;
+use GraphQL\Error\UserError;
+use Overblog\GraphQLBundle\Definition\Argument;
+
+class ObjectStateResolver
+{
+    /** @var \eZ\Publish\API\Repository\ObjectStateService */
+    private $objectStateService;
+
+    /**
+     * @param \eZ\Publish\API\Repository\ObjectStateService $objectStateService
+     */
+    public function __construct(ObjectStateService $objectStateService)
+    {
+        $this->objectStateService = $objectStateService;
+    }
+
+    /**
+     * @param \Overblog\GraphQLBundle\Definition\Argument $args
+     *
+     * @return \eZ\Publish\API\Repository\Values\ObjectState\ObjectState
+     */
+    public function resolveObjectStateById(Argument $args): ObjectState
+    {
+        try {
+            return $this->objectStateService->loadObjectState($args['id']);
+        } catch (NotFoundException $e) {
+            throw new UserError("Object State with ID: {$args['id']} was not found.");
+        }
+    }
+
+    /**
+     * @param \eZ\Publish\API\Repository\Values\ObjectState\ObjectStateGroup $objectStateGroup
+     *
+     * @return \eZ\Publish\API\Repository\Values\ObjectState\ObjectState[]
+     */
+    public function resolveObjectStatesByGroup(ObjectStateGroup $objectStateGroup): array
+    {
+        return $this->objectStateService->loadObjectStates($objectStateGroup);
+    }
+
+    /**
+     * @param \Overblog\GraphQLBundle\Definition\Argument $args
+     *
+     * @return \eZ\Publish\API\Repository\Values\ObjectState\ObjectState[]
+     */
+    public function resolveObjectStatesByGroupId(Argument $args): array
+    {
+        try {
+            $group = $this->objectStateService->loadObjectStateGroup($args['groupId']);
+        } catch (NotFoundException $e) {
+            throw new UserError("Object State Group with ID: {$args['groupId']} was not found.");
+        }
+
+        return $this->objectStateService->loadObjectStates($group);
+    }
+
+    /**
+     * @param \eZ\Publish\API\Repository\Values\Content\ContentInfo $contentInfo
+     *
+     * @return \eZ\Publish\API\Repository\Values\ObjectState\ObjectState[]
+     */
+    public function resolveObjectStateByContentInfo(ContentInfo $contentInfo): array
+    {
+        $objectStates = [];
+        foreach ($this->objectStateService->loadObjectStateGroups() as $group) {
+            $objectStates[] = $this->objectStateService->getContentState($contentInfo, $group);
+        }
+
+        return $objectStates;
+    }
+}
