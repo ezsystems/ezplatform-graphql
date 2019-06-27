4b55f6c1ae2b91941fc9fb82dcaaf6167c5be9d0
Bertrand Dunogier <bertrand.dunogier@ez.no>
2019-06-27 09:45:28 +0200

    Changed the generation command to pick the path from the container (#47)

--- src/Command/GeneratePlatformSchemaCommand.php
+++ src/Command/GeneratePlatformSchemaCommand.php
@@ -22,12 +22,30 @@ class GeneratePlatformSchemaCommand extends Command
      */
     private $generator;
 
+    /**
+     * @var string
+     */
+    private $schemaRootDir;
+
+    /**
+     * @deprecated since v1.1, will be removed in v2.0. Inject the path instead.
+     */
     const TYPES_DIRECTORY = 'app/config/graphql/ezplatform';
 
-    public function __construct(Generator $generator)
+    public function __construct(Generator $generator, ?string $schemaRootDir = null)
     {
         parent::__construct();
         $this->generator = $generator;
+
+        if (null !== $schemaRootDir) {
+            $this->schemaRootDir = $schemaRootDir;
+        } else {
+            $this->schemaRootDir = self::TYPES_DIRECTORY;
+            @trigger_error(
+                'Not specifying $schemaRootDir in ' . __METHOD__ . ' is deprecated since v1.1',
+                E_USER_DEPRECATED
+            );
+        }
     }
 
     protected function configure()
@@ -51,7 +69,7 @@ class GeneratePlatformSchemaCommand extends Command
             if (count($include) && !in_array($type, $include)) {
                 continue;
             }
-            $typeFilePath = self::TYPES_DIRECTORY . "/$type.types.yml";
+            $typeFilePath = $this->schemaRootDir . "/$type.types.yaml";
 
             $yaml = Yaml::dump([$type => $definition], 6);
             if ($doWrite) {
