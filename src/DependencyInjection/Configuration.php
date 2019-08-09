3d9ed0ecafd2f7beac9a0b75984ed8529e41ced6
Adam Wójs <adam.wojs@ez.no>
2019-08-09 15:25:23 +0200

    EZP-30846: Dropped deprecated Symfony\Component\Config\Definition\Builder\TreeBuilder::root method calls

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -21,13 +21,6 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('ezplatform_graphql');
-
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
-
-        return $treeBuilder;
+        return new TreeBuilder('ezplatform_graphql');
     }
 }
