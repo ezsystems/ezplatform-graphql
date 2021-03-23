7f8cd187815c8dc48ca5c277fecd0fef3e3faf67
Mateusz Bieniek <mateusz@bieniek.dev>
2021-03-23 10:51:28 +0100

    CR#2.1

--- src/Schema/Domain/Content/NameHelper.php
+++ src/Schema/Domain/Content/NameHelper.php
@@ -9,21 +9,20 @@ namespace EzSystems\EzPlatformGraphQL\Schema\Domain\Content;
 use eZ\Publish\API\Repository\Values\ContentType\ContentType;
 use eZ\Publish\API\Repository\Values\ContentType\ContentTypeGroup;
 use eZ\Publish\API\Repository\Values\ContentType\FieldDefinition;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
 use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
 use Symfony\Component\Serializer\NameConverter\CamelCaseToSnakeCaseNameConverter;
 
-class NameHelper
+class NameHelper implements LoggerAwareInterface
 {
+    use LoggerAwareTrait;
+
     /**
      * @var \Symfony\Component\Serializer\NameConverter\CamelCaseToSnakeCaseNameConverter
      */
     private $caseConverter;
-
-    /**
-     * @var \Psr\Log\LoggerInterface
-     */
-    private $logger;
 
     /**
      * @var string[]
