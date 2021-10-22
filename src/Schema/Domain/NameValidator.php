743156347f6d1894c9b82239c06c24b5e88b51b2
Adam Wójs <adam@wojs.pl>
2021-10-22 12:03:20 +0200

    IBX-1085: Skipped schema generation for invalid Image Variation (#108)

---
+++ src/Schema/Domain/NameValidator.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\GraphQL\Schema\Domain;
+
+/**
+ * Validates given name according to GraphQL specification. See http://spec.graphql.org/June2018/#sec-Names.
+ */
+final class NameValidator
+{
+    private const NAME_PATTERN = '/^[_a-zA-Z][_a-zA-Z0-9]*$/';
+
+    public function isValidName(string $name): bool
+    {
+        return preg_match(self::NAME_PATTERN, $name) === 1;
+    }
+}
